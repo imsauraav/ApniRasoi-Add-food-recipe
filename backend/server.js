7e6f2bbd52f4c48d32f754d7ff25f7d2284c361d
SAURAV KUMAR <saurav8757462991@gmail.com>
2026-02-26 00:50:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,74 +1,74 @@
 
 
 
-// const express = require("express")
-// const app = express()
-// require("dotenv").config()
-// const connectDb = require("./config/connectionDb")
-// const cors = require("cors")
-
-// const PORT = process.env.PORT || 5000
-
-// connectDb()
-
-// app.use(express.json())
-
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }))
-
-// app.use(express.static("public"))
-
-// // FIXED ROUTES
-// app.use("/user", require("./routes/user"))
-// app.use("/recipe", require("./routes/recipe"))
-
-// app.listen(PORT, () => {
-//   console.log(`app is listening on port ${PORT}`)
-// })
-
-
-
-
-
-
-
 const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-dotenv.config()
-
-const connectDb = require("./config/connectionDb")
-
 const app = express()
+require("dotenv").config()
+const connectDb = require("./config/connectionDb")
+const cors = require("cors")
 
-// CONNECT DB
-connectDb()
-
-// MIDDLEWARES
-app.use(cors({
- origin: "*",
- methods: ["GET","POST","PUT","DELETE"],
- credentials: true
-}))
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-// ROUTES
-app.use("/api/users", require("./routes/user"))
-app.use("/api/items", require("./routes/item"))
-app.use("/api/shops", require("./routes/shop"))
-
-// TEST ROUTE
-app.get("/", (req,res)=>{
- res.send("Backend running successfully 🚀")
-})
-
-// PORT (RENDER)
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
- console.log("Server running on", PORT)
+connectDb()
+
+app.use(express.json())
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+
+app.use(express.static("public"))
+
+// FIXED ROUTES
+app.use("/user", require("./routes/user"))
+app.use("/recipe", require("./routes/recipe"))
+
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`)
 })
+
+
+
+
+
+
+
+// const express = require("express")
+// const cors = require("cors")
+// const dotenv = require("dotenv")
+// dotenv.config()
+
+// const connectDb = require("./config/connectionDb")
+
+// const app = express()
+
+// // CONNECT DB
+// connectDb()
+
+// // MIDDLEWARES
+// app.use(cors({
+//  origin: "*",
+//  methods: ["GET","POST","PUT","DELETE"],
+//  credentials: true
+// }))
+
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+// // ROUTES
+// app.use("/api/users", require("./routes/user"))
+// app.use("/api/items", require("./routes/item"))
+// app.use("/api/shops", require("./routes/shop"))
+
+// // TEST ROUTE
+// app.get("/", (req,res)=>{
+//  res.send("Backend running successfully 🚀")
+// })
+
+// // PORT (RENDER)
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, ()=>{
+//  console.log("Server running on", PORT)
+// })
